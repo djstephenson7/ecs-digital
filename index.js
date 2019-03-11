938bb25eff293771536562cfdbeac68fb2c80e17
djstephenson7 <dan_0502@hotmail.co.uk>
2019-03-11 18:48:09 +0000

    Passed test

--- index.js
+++ index.js
@@ -9,19 +9,18 @@ const data = [
   'parent4',
 ];
 
-const parent1 = data.filter(data => data.includes('parent1/')).map(d => d.replace('parent1/', ''));
-const parent2 = data.filter(data => data.includes('parent2/')).map(d => d.replace('parent2/', ''));
-const parent3 = data.filter(data => data.includes('parent3/')).map(d => d.replace('parent3/', ''));
-
-
-function createMenuData(parent1, parent2, parent3) {
+function createMenuData(data) {
+  const parent1 = data.filter(data => data.includes('parent1/')).map(d => d.replace('parent1/', ''));
+  const parent2 = data.filter(data => data.includes('parent2/')).map(d => d.replace('parent2/', ''));
+  const parent3 = data.filter(data => data.includes('parent3/')).map(d => d.replace('parent3/', ''));
   const obj = [
     { title: 'parent1', data: parent1 },
     { title: 'parent2', data: parent2 },
     { title: 'parent3', data: parent3 },
   ];
-
-  console.log(obj);
+  return obj;
 }
 
-createMenuData(parent1, parent2, parent3);
+createMenuData(data);
+
+module.exports = createMenuData;
