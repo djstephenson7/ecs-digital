4618fcda86bbd13a48b3b06cd7a322177ac2b208
djstephenson7 <dan_0502@hotmail.co.uk>
2019-03-11 18:40:07 +0000

    Linted and refactored

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ const data = [
   'parent4',
 ];
 
-const obj = [
-  { title: 'parent1', data: 1 },
-  { title: 'parent2', data: 2 },
-  { title: 'parent3', data: 3 },
-];
-
 const parent1 = data.filter(data => data.includes('parent1/')).map(d => d.replace('parent1/', ''));
 const parent2 = data.filter(data => data.includes('parent2/')).map(d => d.replace('parent2/', ''));
 const parent3 = data.filter(data => data.includes('parent3/')).map(d => d.replace('parent3/', ''));
@@ -27,9 +21,7 @@ function createMenuData(parent1, parent2, parent3) {
     { title: 'parent3', data: parent3 },
   ];
 
-  console.log(obj)
-  // console.log(parent2)
-  // console.log(parent3)
+  console.log(obj);
 }
 
-createMenuData(parent1, parent2, parent3)
+createMenuData(parent1, parent2, parent3);
